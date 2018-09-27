24873bba7bdf22b22479af2e5def3f45a2c91843
Metacome1 <igordarmin123@gmail.com>
2018-09-28 01:26:06 +0400

    Added entry point

---
+++ src/RPIS61/Darmin/wdad/data/Application.java
@@ -0,0 +1,9 @@
+package RPIS61.Darmin.wdad.data;
+
+public class Application {
+
+    public static void main(String[] args){
+        System.out.println("I'am Igor Darmin,  and I’m not a monkey");
+
+    }
+}
